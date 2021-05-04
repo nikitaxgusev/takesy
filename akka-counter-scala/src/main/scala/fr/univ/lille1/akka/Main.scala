65a6592d8bd42bf28bb734cde759a4353c3941d4
Korristo <nekit01031996@mail.ru>
2021-05-04 23:55:34 +0300

    The first prototype: Need to finalize our TODOs

--- akka-counter-scala/src/main/scala/fr/univ/lille1/akka/Main.scala
+++ akka-counter-scala/src/main/scala/fr/univ/lille1/akka/Main.scala
@@ -1,45 +1,56 @@
-package fr.univ.lille1.akka
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import scala.io.Source
 
-import akka.actor.{ActorRef, ActorSystem, Props}
-import fr.univ.lille1.akka.actors._
-import scala.io.StdIn
+object AssignmentPart1 extends App {
 
-object Main extends App {
+  //Define case classes so CountActor knows what format method should be in
+  case class readFromFile(actorRef0: ActorRef, actorRef1: ActorRef, source_file : scala.io.BufferedSource)
+  // TODO: rename methods
+  case class StartCounting(actorRef: ActorRef, str : String)
+  case class CountSecond(s : String)
+  case class Combine(final_str : String)
 
-  println("[INFO] System creation : akka-counter-scala")
-  val system: ActorSystem = ActorSystem("akka-counter-scala")
+  class CountActor extends Actor {
+    def receive: Receive = {
+      //read from file for every line
+      // launch chain of actors (0,1,2) for every line
+      case readFromFile(actor1, actor2, source_file) =>
+        for (line <- source_file.getLines()) {
+          actor1 ! StartCounting(actor2, line)
+        }
 
-  var textFiles = Map(1 -> "text.txt", 2 -> "small-text.txt")
-  var choiceTextFile: Int = 1
-  var typeCounter: Int = 1
-  var occurenceToFind: String = null
-  var numberOfWorkers: Int = 1
+      // this actor marks a file by spevific labels
+      case StartCounting(actor2, s) =>
+        //TODO: take line, set label and return string with label
+        actor2 ! CountSecond(s)
 
-  println("[MENU] Welcome to the menu")
-//  try {
-//    println("Which text do you want to analyze? (default 1)")
-//    for ((key,value) <- textFiles) printf("\t%s) %s\n", key, value)
-//    this.choiceTextFile = StdIn.readInt()
-//    if(this.choiceTextFile < 1 || this.choiceTextFile > textFiles.size)
-//      throw new NumberFormatException
-//  } catch {
-//    case e: NumberFormatException => {
-//      println("[ERROR] Incorrect value, we took the default choice")
-//      this.choiceTextFile = 1
-//    };
-//  }
-  try {
-    println("How many workers do you want (default 1) ?")
-    this.numberOfWorkers = StdIn.readInt()
-  } catch {
-    case e: NumberFormatException => {
-      println("[ERROR] Incorrect value, we took the default choice")
-      this.numberOfWorkers = 1
-    };
+      // Modify marked string by rule and return it to final processing
+      case CountSecond(s) =>
+        var tmp_str = s
+        if (tmp_str.contains("int")) {
+          tmp_str = s.replace("int", "float")
+        }
+        // TODO: 1. add more changes, like data types: float , int, char ,string
+        //       2. dont read exceptions: return , int main and etc
+        sender() ! Combine(tmp_str)
+
+      // Write in file the final result line by line
+      case Combine(final_str) =>
+        // TODO: create a new file , write a line
+        println(final_str)
+    }
   }
 
-  println("[INFO] Creation & configuration of the master")
-  val master: ActorRef = system.actorOf(Props(new Master(this.numberOfWorkers, occurenceToFind)))
+  //Store content from txt file into a String
+  val source_file = Source.fromFile("text.txt")
 
-  master ! textFiles(choiceTextFile)
+  //Create the actor system and actors, and send a message to CountActor with a method
+  val system = ActorSystem("count")
+  val actor0 = system.actorOf(Props[CountActor], name = "count-actor0")
+  val actor1 = system.actorOf(Props[CountActor], name = "count-actor1")
+  val actor2 = system.actorOf(Props[CountActor], name = "count-actor2")
+
+  actor0 ! readFromFile(actor1, actor2, source_file)
+
+  system.terminate()
 }
